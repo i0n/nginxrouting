f752884f9e3cec99f4e8cdaa987d6262030458ff
rujmah <rjjm@rjjm.net>
2012-08-07 15:34:43 +0100

    added headers to the output of test

--- scripts/test_routes.rb
+++ scripts/test_routes.rb
@@ -10,11 +10,10 @@ Script takes a file containing urls and runs them against a specified domain.
 Don't use any of the these options if you want to run the default test.
 
 EOS
-	opt :file, "Spreadsheet (xls) to take the URLs from", :short => "f", :default => 'bizlink_ip_mappings.xls'
+	opt :file, "Spreadsheet (xls) to take the URLs from", :short => "f", :default => 'spreadsheets/bizlink_ip_mappings.xls'
 	opt :column, "Column to take the URLs from (if more than one column in spreadsheet) - start at 0", :short => "c", :default => 0
 	opt :domain, "The domain to convert the urls to (e.g. entering 'google.com' will replace 'http://yahoo.com/search=this' with 'http://google.com/search=this' )", :short => 'd'
 	
-
 end
 
 # get domain
@@ -22,38 +21,16 @@ idom = ARGV[0] if opts[:domain]
 puts "URLs will be converted to use this domain: #{idom}" if idom
 
 # get file
-puts opts[:file]
+puts "Spreadsheet: #{opts[:file]}"
 
 # get column
-puts opts[:column]
-
-# get input
-# domain
-
-
-
-# the following code runs against Ben's spreadsheet.
-# find and download it and change the name of openned spreadsheet to use.
-
+puts "Column: #{opts[:column]}"
 
 # open excel spreadsheet from Ben
 book = Spreadsheet.open opts[:file]
 # get first worksheet
 front = book.worksheet 0
 
-if false
-	# collection of old_url, clean_url and govuk_url
-	collected = []
-	# uses function from spreadsheet gem to exclude the first row
-	front.each 1 do |row|
-		collected << [
-			row[2], row[3], row[11]
-		]
-	end
-
-old_urls = collected.map { |e| e[0] }
-
-end
 
 # array of old_urls only
 old_urls = []
@@ -67,7 +44,7 @@ def revise_url idom=false, url
 		url
 	else
 		# not perfect, but does the job for the moment
-		url.gsub(/\/\/.*\//, "//#{idom}/")
+		url.gsub(/\/\/.*?\//, "//#{idom}/")
 	end
 end
 
@@ -83,14 +60,29 @@ old_urls[0..2].each do |u|
 			op.unshift 'alive'
 		when 302, 301
 			op.unshift 'redirected'
-		when 400 
+		when 400, 404 
 			op.unshift 'not found'
 		else 
 			op.unshift 'unrecognised'
 		end
 
+		op << response.headers
 		puts op.to_s
 	}
 end
 
 
+
+if false
+	# collection of old_url, clean_url and govuk_url
+	collected = []
+	# uses function from spreadsheet gem to exclude the first row
+	front.each 1 do |row|
+		collected << [
+			row[2], row[3], row[11]
+		]
+	end
+
+	old_urls = collected.map { |e| e[0] }
+
+end
